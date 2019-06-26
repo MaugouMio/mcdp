2f62161e2e80f80e55230a2162766529a69cbfdc
MaugouMio <maugoumine@gmail.com>
2019-06-26 09:10:40 +0800

    complete most of the script reading structure

--- mcdp.exe.py
+++ mcdp.exe.py
@@ -1,14 +1,41 @@
 # -*- coding: UTF-8 -*-
 import sys, os
+from io import StringIO
 
-install_path = os.path.expandvars("%LOCALAPPDATA%\\mcdp")
-module_path = ["./", install_path + "\\lib\\"]
+install_path = os.path.expandvars("%LOCALAPPDATA%/mcdp")
+module_path = [install_path + "/lib/"]
 
 class Folder:
 	def __init__(self):
 		self.items = dict()
 		self.virtual = False
+		
+	def copy(self):
+		new_folder = Folder()
+		for key in self.items.keys():
+			new_folder.items[key] = self.items[key].copy()
+		new_folder.virtual = self.virtual
+		
+		return new_folder
+		
+	def build(self):
+		pass
+		
+class Function:
+	def __init__(self):
+		self.commands = ""
+		self.virtual = False
 
+	def copy(self):
+		new_function = Function()
+		new_function.commands = self.commands
+		new_function.virtual = self.virtual
+		
+		return new_function
+		
+	def build(self):
+		pass
+		
 
 
 sep_char = [' ', ',']
@@ -36,70 +63,186 @@ def merge_two_dicts(x, y):
 #            normal functions             #
 ###########################################
 
-def find_lib(script_name):
-	for path in module_path:
+def find_lib(script_name, search_path):
+	for path in search_path:
+		# fix directory path
+		if path[-1] != '/' and path[-1] != '\\':
+			path += '/'
+			
 		if os.path.isfile(path + script_name + "dpl"):
-			return path + script_name + "dpl"
+			with open(path + script_name + "dpl", "r", encoding='UTF-8') as f:
+				script = f.read().splitlines()
+			return script, path
+		elif os.path.isfile(path + script_name + "/__main__.dpl"):
+			with open(path + script_name + "/__main__.dpl", "r", encoding='UTF-8') as f:
+				script = f.read().splitlines()
+			return script, path + script_name
+			
 	raise ImportError("No module named '" + script_name + "'")
-	
-def read_script(script):
-	namespaces = {}
 
+def parse_code(code):
+	old_stdout = sys.stdout
+	redirected_output = sys.stdout = StringIO()
+	exec(code)
+	sys.stdout = old_stdout
+	
+	return redirected_output.getvalue()
+
+def read_block(code, this, args): # 'this' is a new Folder() or Function() object
+	# replace arguments
+	# read sub blocks
+	pass
+
+def parse_definition(line):
+	end_index = line.find(' ') + 1
+	
+	return declare_name, argv, template, is_virtual, end_index
+
+def add_namespace(name, namespace, namespaces):
+	if name in namespaces.keys() and not namespaces[name].virtual:
+		raise NameError("namespace " + name + " already defined!")
+	
+	namespaces[name] = namespace
+	
+def read_script(script, this_dir):
+	search_path = [this_dir] + module_path
+	
+	namespaces = {}
+	description = ""
+
+	block_stack = 0
+	block_content = ""
+	this_block = None
+	
+	in_pycode = False
+	pycode = ""
+	
 	line_index = 0
 	while line_index < len(script):
-		this_line = script[line_index].lstrip('\t')
-		# delete comments
-		if this_line[0] == '#':
-			del script[line_index]
+		# parsing python codes
+		if in_pycode:
+			if script[line_index].rstrip(" \t") == "```":
+				parsed_pycode = parse_code(pycode).split('\n')
+				for i in range(len(parsed_pycode)):
+					script.insert(line_index + 1, parsed_pycode[len(parsed_pycode) - i - 1])
+				# reset stats
+				in_pycode = False
+				pycode = ""
+			else:
+				pycode += script[line_index] + '\n'
+				line_index += 1
 		else:
-			splitted_line = split_by_chars(this_line)
-			# not an empty row
-			if len(splitted_line) > 0:
-				if splitted_line[0] == "import":
-					if len(splitted_line) < 2:
-						raise SyntaxError(this_line)
-						
-					imported = read_script(find_lib(splitted_line[1]))
-					if len(splitted_line) == 4 and splitted_line[2] == "as" and splitted_line[3] == "virtual":
-						# set namespaces as virtual
-						for key in imported.keys():
-							imported[key].virtual = True
-					elif len(splitted_line) != 2:
-						raise SyntaxError(this_line)
-					
-					for key in imported.keys():
-						if key in namespaces.keys() and namespaces[key].virtual == False:
-							raise NameError("namespace " + key + " already defined!")
-							
-					merge_two_dicts(namespaces, imported)
-				elif splitted_line[0] == "from":
-					if len(splitted_line) < 4 or splitted_line[2] != "import":
-						raise SyntaxError(this_line)
-					
-					# delete unneeded namespaces
-					imported = read_script(find_lib(splitted_line[1]))
-					imported_namespaces = list(imported.keys())
-					for key in imported_namespaces:
-						if key in splitted_line[3:-2]:
-							imported.pop(key)
-					
-					if splitted_line[-2] == "as" and splitted_line[-1] == "virtual":
-						# set namespaces as virtual
-						for key in imported.keys():
-							imported[key].virtual = True
-					
-					merge_two_dicts(namespaces, imported)
-				elif splitted_line
+			this_line = script[line_index].replace('\t', '')
+			# delete comments
+			if this_line[0] == '#':
+				del script[line_index]
+			else:
+				# reading block contents
+				if block_stack > 0:
+					# deal with block stack
+					# copy this line to block_content (without the last '}')
+					pass
 				
-	return namespaces
+				# parsing contents by lines
+				else:
+					splitted_line = split_by_chars(this_line)
+					# not an empty row
+					if len(splitted_line) > 0:
+						# import module
+						if splitted_line[0] == "import":
+							if len(splitted_line) < 2:
+								raise SyntaxError(this_line)
+								
+							imported = read_script(find_lib(splitted_line[1], search_path))
+							if len(splitted_line) == 4 and splitted_line[2] == "as" and splitted_line[3] == "virtual":
+								# set namespaces as virtual
+								for key in imported.keys():
+									imported[key].virtual = True
+							elif len(splitted_line) != 2:
+								raise SyntaxError(this_line)
+							
+							for key in imported.keys():
+								if key in namespaces.keys() and namespaces[key].virtual == False:
+									raise NameError("namespace " + key + " already defined!")
+									
+							merge_two_dicts(namespaces, imported)
+							
+							
+							
+						# from module import namespaces
+						elif splitted_line[0] == "from":
+							if len(splitted_line) < 4 or splitted_line[2] != "import":
+								raise SyntaxError(this_line)
+							
+							# delete unneeded namespaces
+							imported = read_script(find_lib(splitted_line[1], search_path))
+							imported_namespaces = list(imported.keys())
+							for key in imported_namespaces:
+								if key in splitted_line[3:-2]:
+									imported.pop(key)
+							
+							if splitted_line[-2] == "as" and splitted_line[-1] == "virtual":
+								# set namespaces as virtual
+								for key in imported.keys():
+									imported[key].virtual = True
+							
+							merge_two_dicts(namespaces, imported)
+							
+							
+							
+						# define a namespace (blocks are separated by {} instead of LF)
+						elif splitted_line[0] == "namespace":
+							declare_name, argv, template, is_virtual, end_index = parse_definition(this_line)
+							
+							if (end_index + 1) == len(this_line):
+								this_block = Folder()
+								block_stack += 1
+								
+								# preview and ignore the next line
+								line_index += 1
+								if script[line_index].lstrip(" \t").rstrip(" \t") != '{':
+									raise SyntaxError(script[line_index])
+							else:
+								rest_contents = this_line[(end_index + 1):].lstrip(" \t").rstrip(" \t")
+								if rest_contents == '{':
+									block_stack += 1
+								elif rest_contents == ';':
+									new_namespace = template.copy()
+									new_namespace.virtual = is_virtual
+									add_namespace(declare_name, new_namespace, namespaces)
+								else:
+									raise SyntaxError(this_line)
+						
+						
+						
+						# python codes to generate contents
+						elif splitlines[0] == "```":
+							in_pycode = True
+						
+						
+						
+						# add minecraft tags
+						elif splitlines[0] == "tag":
+							pass
+						
+						
+						
+						# unknown line
+						else:
+							raise SyntaxError(this_line)
+						
+						line_index += 1
+						
+	return description, namespaces
 
 def build_datapack():
 	filename = sys.argv[2]
 	if filename[-4:] != ".dpl":
 		filename += ".dpl"
 	
+	this_dir = os.path.dirname(os.path.abspath(filename))
 	with open(filename, "r", encoding='UTF-8') as script:
-		namespaces = read_script(script.read().splitlines())
+		description, namespaces = read_script(script.read().splitlines(), this_dir)
 	
 def install_module():
 	pass
